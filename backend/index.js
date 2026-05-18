b1d6dbf1052ea01d64b4e5122021ba8101f3a4af
Viktoria Loiko <viktoria.loiko@ivkhk.ee>
2026-05-18 18:04:47 +0300

    Initial project structure

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
